e565e36426fcefaddd31a554416ff9ad3d329727
miesnerjacob <miesner.jacob@gmail.com>
2024-12-08 12:29:57 -0500

    added example for simple value types and manipulation

---
+++ 02_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Strings
+	fmt.Println("Hello" + ", " + "World!")
+
+	// Integers
+	fmt.Println("10 * 2 =", 10*2)
+	
+	// Floats
+	fmt.Println("10 / 3 =", 10/3)
+	
+	// Booleans
+	fmt.Println("false && true =", false && true)
+	fmt.Println("false || true =", false || true)
+	fmt.Println("!false =", !false)
+
+}
+
